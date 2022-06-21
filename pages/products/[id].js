6db4e2dc2ee76e7f2f441dde1c2b59bf791f7538
rochafialvin <rochafi.dev@gmail.com>
2022-06-21 11:51:26 +0700

    Create detail page

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,10 +1,32 @@
-import { useRouter } from "next/router";
+import { Box, HStack, Flex, Text } from "@chakra-ui/react";
+import axiosInstance from "../../services/axios";
 
-function ProductDetail() {
-  const router = useRouter();
-  const { id } = router.query;
+function ProductDetail(props) {
+  const { product } = props;
+  const { productName, productImage, price, description } = product;
 
-  return <div>Product Detail : {id}</div>;
+  return (
+    <HStack>
+      <img style={{ width: "50%" }} src={productImage} alt={productName} />
+      <Box p={2}>
+        <Text mb={3}>{productName}</Text>
+        <Text mb={2}>{description}</Text>
+        <Text>Rp. {price.toLocaleString("id")}</Text>
+      </Box>
+    </HStack>
+  );
+}
+
+export async function getServerSideProps(context) {
+  try {
+    const { id } = context.params;
+    const res = await axiosInstance.get(`/products/${id}`);
+    return {
+      props: { product: res.data },
+    };
+  } catch (error) {
+    console.log({ error });
+  }
 }
 
 export default ProductDetail;
