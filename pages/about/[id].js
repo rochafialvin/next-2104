691083a014264ee218605eb35d09d6a475dccb48
rochafialvin <rochafi.dev@gmail.com>
2022-06-21 10:24:43 +0700

    Using getStaticPaths

--- pages/about/[id].js
+++ pages/about/[id].js
@@ -1,5 +1,39 @@
-import React from "react";
+import axiosInstance from "../../services/axios";
 
-export default function AboutDetail() {
-  return <div>AboutDetail</div>;
+export default function AboutDetail(props) {
+  const { chief } = props;
+
+  if (!chief) return <h1>loading ...</h1>;
+
+  return <div>Detail {chief.name} </div>;
+}
+
+export async function getStaticProps(context) {
+  try {
+    const { id } = context.params;
+    const res = await axiosInstance.get(`/chiefs/${id}`);
+    return {
+      props: { chief: res.data },
+    };
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function getStaticPaths() {
+  try {
+    const res = await axiosInstance.get("chiefs");
+    const chiefs = res.data;
+    const pathParams = chiefs.map((chief) => {
+      return { params: { id: String(chief.id) } };
+    });
+
+    return {
+      // paths akan menentukan jumlah page yang akan digenerate
+      paths: pathParams,
+      fallback: true, // false or 'blocking'
+    };
+  } catch (error) {
+    console.log({ error });
+  }
 }
