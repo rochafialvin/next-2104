01c648a91517a4a2f75110a6663d47bffd611bbb
rochafialvin <rochafi.dev@gmail.com>
2022-06-21 11:25:07 +0700

    Render products

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,7 +1,27 @@
 import React from "react";
+import ProductItem from "../../components/product-item";
+import axiosInstance from "../../services/axios";
 
-function Products() {
-  return <div>Products List</div>;
+function Products(props) {
+  const { products } = props;
+
+  const renderProducts = () => {
+    return products.map((product) => <ProductItem product={product} />);
+  };
+
+  return <div>{renderProducts()}</div>;
+}
+
+export async function getServerSideProps() {
+  try {
+    const res = await axiosInstance.get("/products");
+
+    return {
+      props: { products: res.data },
+    };
+  } catch (error) {
+    console.log({ error });
+  }
 }
 
 export default Products;
