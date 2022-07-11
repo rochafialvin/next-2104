87142f7731578913e2b5dc7917b1b9a8e14ab6d3
rochafialvin <rochafi.dev@gmail.com>
2022-07-11 13:34:36 +0700

    Finish checkout function

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -4,71 +4,109 @@ import Image from "next/image";
 import React from "react";
 import { api_origin } from "../../constraint";
 import axiosInstance from "../../services/axios";
+import { useRouter } from "next/router";
 
 function Cart(props) {
   const { carts, total } = props;
+  const router = useRouter();
 
-  const renderProducts = () => {
-    return carts.map((cart) => {
-      const { cart_id, quantity, variant, price, origin, image, total } = cart;
-      return (
-        <Flex
-          key={cart_id}
-          w="100%"
-          border="1px solid gray"
-          borderRadius={6}
-          marginBlock={3}
-        >
-          <Box
-            w="25%"
-            display={"flex"}
-            justifyContent="center"
-            alignItems="center"
+  const onCheckoutClick = async () => {
+    try {
+      const body = {
+        total,
+        products: carts,
+      };
+      const session = await getSession();
+
+      const { accessToken } = session.user;
+
+      const config = {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      };
+
+      const res = await axiosInstance.post("/transactions", body, config);
+
+      alert(res.data.message);
+
+      router.replace("/transaction");
+    } catch (error) {
+      console.log({ error });
+    }
+  };
+
+  const renderCart = () => {
+    if (carts)
+      return carts.map((cart) => {
+        const { cart_id, quantity, variant, price, origin, image, total } =
+          cart;
+        return (
+          <Flex
+            key={cart_id}
+            w="100%"
+            border="1px solid gray"
+            borderRadius={6}
+            marginBlock={3}
           >
-            <Image
-              src={api_origin + image}
-              alt={variant}
-              width={70}
-              height={70}
-            />
-          </Box>
-          <Box borderLeft="1px solid gray" w="50%" p={5}>
-            <Text fontWeight="bold" fontSize="1.2em">
-              Variant : {variant}
-            </Text>
-            <Text>Origin : {origin}</Text>
-            <Text>Quantity : {quantity}</Text>
-            <Text>Rp.{price.toLocaleString("id")}</Text>
-          </Box>
-          <Box
-            borderLeft="1px solid gray"
-            w="25%"
-            p={5}
-            display={"flex"}
-            flexDirection="column"
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Text fontWeight="bold" fontSize="1.2em">
-              Total
-            </Text>
-            <Text fontWeight="bold" fontSize="1em">
-              {total}
-            </Text>
-          </Box>
-        </Flex>
-      );
-    });
+            <Box
+              w="25%"
+              display={"flex"}
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Box
+                border="1px solid gray"
+                width={70}
+                height={70}
+                rounded={6}
+                overflow="hidden" // agar image tidak keluar dari border
+              >
+                <Image
+                  src={api_origin + image}
+                  alt={variant}
+                  width={70}
+                  height={70}
+                />
+              </Box>
+            </Box>
+            <Box borderLeft="1px solid gray" w="50%" p={5}>
+              <Text fontWeight="bold" fontSize="1.2em">
+                Variant : {variant}
+              </Text>
+              <Text>Origin : {origin}</Text>
+              <Text>Quantity : {quantity}</Text>
+              <Text>${price}</Text>
+            </Box>
+            <Box
+              borderLeft="1px solid gray"
+              w="25%"
+              p={5}
+              display={"flex"}
+              flexDirection="column"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Text fontWeight="bold" fontSize="1.2em">
+                Total
+              </Text>
+              <Text fontWeight="bold" fontSize="1em">
+                ${total}
+              </Text>
+            </Box>
+          </Flex>
+        );
+      });
+
+    return <Text>Cart is empty</Text>;
   };
 
   return (
     <Box>
-      <Box>{renderProducts()}</Box>
+      <Box>{renderCart()}</Box>
       <Flex justifyContent={"space-around"}>
         <Text fontWeight="bold" fontSize="1.5em">
-          Total : {total}
+          Total : ${total}
         </Text>
-        <Button variant="ghost" w="50%">
+        <Button variant="ghost" w="50%" onClick={onCheckoutClick}>
           Checkout
         </Button>
       </Flex>
